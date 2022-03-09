ed65022178a0b736e41524804326b3ae83237b77
Gilbert Huarcaya <gilberthuarcaya@gmail.com>
2022-03-08 19:51:56 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import { SizeSelector } from "./components/SizeSelector";
+
+function App() {
+  const [currentSize, setCurrentSize] = useState("none");
+
+  return (
+    <div className="App App-header">
+      <h1>Talla seleccionada: {currentSize}</h1>
+      <SizeSelector
+        selectedSize={currentSize}
+        onSizeChange={setCurrentSize}></SizeSelector>
+    </div>
+  );
+}
+export default App;
